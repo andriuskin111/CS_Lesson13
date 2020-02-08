4c38f2e12b4f9ddd307e2868129c988b07af9ad6
andriuskin111 <andriuskin@gmail.com>
2020-02-08 23:07:35 +0200

    Initiate

---
+++ CompanyManager/CompanyManager.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CompanyManager.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CompanyManager.test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2c50fec3-1918-45eb-9e7e-d44c97d0d0e2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CompanyManager/CompanyManagerBusinesLogicLayer/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyManagerBusinesLogicLayer
+{
+    public class Company
+    {
+        public int CompanyId { get; private set; }
+        public string CompanyName { get; private set; }
+        public List<string> PhoneNumbers;
+
+        public Company(int companyId, string companyName)
+        {
+            CompanyId = companyId;
+            CompanyName = companyName;
+            PhoneNumbers = new List<string>();
+        }
+    }
+}
